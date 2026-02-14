419bc34206cc5ecdb9d8fa7de335560e2ba6591e
HBarrera25 <00226325@uca.edu.sv>
2026-02-14 11:16:47 -0600

    add: crear estructura NodoJugador

---
+++ funciones.h
@@ -0,0 +1,20 @@
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+
+using namespace std;
+
+struct NodoJugador
+{
+    string nombre;
+    string id;
+    int puntos;
+    NodoJugador *sig;
+};
+
+#endif
