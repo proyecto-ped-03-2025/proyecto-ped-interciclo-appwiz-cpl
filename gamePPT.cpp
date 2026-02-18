531baf9dd0b186760ea465f1f0d2a5eb6c52d843
RosGuadalupeEscobar <00235325.uca.edu.sv>
2026-02-17 23:11:27 -0600

    add: Estructura menu principal

---
+++ gamePPT.cpp
@@ -0,0 +1,56 @@
+#include "funciones.h"
+
+int main()
+{
+    srand(time(NULL));
+
+    GamePPT juego;
+
+    int opcion;
+
+    do
+    {
+        juego.limpiar();
+        juego.titulo();
+
+        cout << "\n===== MENU =====\n";
+        cout << "1. Agregar jugadores\n";
+        cout << "2. Ver lista jugadores\n";
+        cout << "3. Iniciar juego\n";
+        cout << "4. Ver ganador\n";
+        cout << "5. Salir\n";
+        cout << "Seleccione opcion: ";
+        cin >> opcion;
+
+        switch (opcion)
+        {
+        case 1:
+            juego.inscribirJugador();
+            break;
+
+        case 2:
+            juego.listaJugadores();
+            break;
+
+        case 3:
+            juego.ejecutarJuego();
+            break;
+
+        case 4:
+            juego.mostrarGanador();
+            break;
+
+        case 5:
+            cout << "Saliendo...\n";
+            juego.liberarMemoria();
+            break;
+
+        default:
+            cout << "Opcion invalida\n";
+            juego.pausa();
+        }
+
+    } while (opcion != 5);
+
+    return 0;
+}
